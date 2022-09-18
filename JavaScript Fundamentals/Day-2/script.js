03462573f38bed836e950a2c56c20943b30afd5c
Biswajit Adhikary <biswajitadhikary.web@gmail.com>
2022-09-19 00:52:24 +0600

    New Folder Added

---
+++ JavaScript Fundamentals/Day-2/script.js
@@ -0,0 +1,9 @@
+console.log("Adrita Adhikary");
+let firstName = "Adrita";
+console.log(firstName);
+let country = "Bangladesh";
+let continent = "Khulna";
+let population = "165M";
+console.log(country);
+console.log(continent);
+console.log(population);
